c3db9082db04a591cfd251eb5271e039646062db
AdryKab47 <luiscabreraucme@hotmail.com>
2024-04-20 23:51:55 -0500

    Insertar Registros

--- Seccion -2 Ambiente de Pruebas/SentenciasSQL.sql
+++ Seccion -2 Ambiente de Pruebas/SentenciasSQL.sql
@@ -1,6 +1,15 @@
--- Adrián Cabrera --
--- Seguimiento curso SQL DevTalles -- 
+-- Adri á n Cabrera -- -- Seguimiento curso SQL DevTalles 
+-- create table users (name VARCHAR(10) unique);
 
-create table users (
-	name VARCHAR(10) unique
-);
+
+select * from users u 
+
+-- Usar comillas simples
+
+INSERT INTO USERS (name) values ('Kurt'); 
+INSERT INTO USERS (name) values ('Axl');
+INSERT INTO USERS (name) values ('Mike');
+INSERT INTO USERS (name) values ('Billie');
+INSERT INTO USERS (name) values ('Enrique');
+-- Se puede o no especificar las columnas de la tabla, pero se debe saber el orden
+INSERT INTO USERS values ('John');
