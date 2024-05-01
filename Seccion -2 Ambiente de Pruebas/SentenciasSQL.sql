b26d8e089d1b012ec91d780ddad5bdac2e6f1e11
AdryKab47 <luiscabreraucme@hotmail.com>
2024-05-01 01:03:49 -0500

    Seleccionar Registros

--- Seccion -2 Ambiente de Pruebas/SentenciasSQL.sql
+++ Seccion -2 Ambiente de Pruebas/SentenciasSQL.sql
@@ -11,5 +11,30 @@ INSERT INTO USERS (name) values ('Axl');
 INSERT INTO USERS (name) values ('Mike');
 INSERT INTO USERS (name) values ('Billie');
 INSERT INTO USERS (name) values ('Enrique');
+
+INSERT INTO USERS (name) values ('Fernanda');
+
 -- Se puede o no especificar las columnas de la tabla, pero se debe saber el orden
 INSERT INTO USERS values ('John');
+
+-- Actualización de Registros
+
+UPDATE USERS 
+SET 
+	"name" = 'Bob Dylan'
+WHERE "name" = 'John' 
+
+
+-- Es recomendable desactivar el auto commit
+
+-- Para listar todos los registros de una tabla 
+SELECT * FROM USERS;
+
+-- Para limitar todos los registros que se muestran obtenidos de un select
+
+SELECT * FROM USERS LIMIT 10;
+
+-- Para limiar los registros y ademas saltarme registros se hace uso del OFFSET
+
+SELECT * FROM USERS LIMIT 2 OFFSET 2;
+
