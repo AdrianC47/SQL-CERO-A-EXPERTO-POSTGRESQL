2532aed6f19e4cc2ed95592bdb330fb204f28e45
AdryKab47 <luiscabreraucme@hotmail.com>
2024-05-05 12:46:32 -0500

    Max Min Count Round AVG

--- Seccion_4_Funciones_Agregadas_Agrupaciones_y_Ordenamiento/Seccion4-Funciones-Agregadas-Agrupaciones-y-Ordenamiento.sql
+++ Seccion_4_Funciones_Agregadas_Agrupaciones_y_Ordenamiento/Seccion4-Funciones-Agregadas-Agrupaciones-y-Ordenamiento.sql
@@ -11,3 +11,25 @@ WHERE
     and 4700
 order by
     followers ASC;
+
+
+-- MAX MIN COUNT ROUND AVG
+SELECT COUNT(*) FROM USERS;
+
+SELECT
+    count(*) as total_users,-- Saber el total de registros
+    min(followers) as min_followers, -- N° de seguidores que tiene como minimo
+    max(followers) as max_followers -- N° de seguidores que tiene como maximo
+FROM
+    USERS;
+    
+
+select * from users;
+-- Se requiere saber cual es el promedio de seguidores que se tiene
+select
+    AVG(followers) as avg_followers,--promedio
+    ROUND(AVG(followers)) as avg_followers_redondeado,--promedio
+    SUM(followers)/count(*) as avg_followers_manual -- promedio manual
+from
+    users;
+
