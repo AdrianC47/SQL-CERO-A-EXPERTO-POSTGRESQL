83dc95222d7a7c16c038518c4819c799d0067b89
AdryKab47 <luiscabreraucme@hotmail.com>
2024-05-25 02:25:45 -0500

    Constraint Check

--- Seccion_5_Relaciones_Llaves_Constraints/Seccion_5_Relaciones_Llaves_Constraints.sql
+++ Seccion_5_Relaciones_Llaves_Constraints/Seccion_5_Relaciones_Llaves_Constraints.sql
@@ -4,3 +4,14 @@ alter table
     country
 add
     PRIMARY key (code);
+
+
+-- * Recodar que si permitimos basura en nuestra base, pues basura vamos a tener
+-- Añadir un Constraint (Check)
+-- Se requiere añadir una regla para los valores de la columna surfacearea que sean solo 
+-- positivos
+select * from country where code = 'ABW';
+
+ALTER TABLE country add CHECK (
+	surfacearea >= 0
+);
