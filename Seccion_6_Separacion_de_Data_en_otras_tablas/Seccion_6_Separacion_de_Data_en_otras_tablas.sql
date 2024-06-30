809034b5fd9d6b57178eb79d0535b9358e5022e5
AdryKab47 <luiscabreraucme@hotmail.com>
2024-06-30 14:15:41 -0500

    Respaldo, Checks y respaldo de country

--- Seccion_6_Separacion_de_Data_en_otras_tablas/Seccion_6_Separacion_de_Data_en_otras_tablas.sql
+++ Seccion_6_Separacion_de_Data_en_otras_tablas/Seccion_6_Separacion_de_Data_en_otras_tablas.sql
@@ -10,3 +10,33 @@ INSERT INTO CONTINENT (name)
         continent ASC
 )
 
+
+-----------------------------------------------------------------------------------------------------------
+
+-- Se crea una tabla para backup
+
+-- Table Definition
+CREATE TABLE "public"."country_bk" (
+    "code" bpchar(3) NOT NULL,
+    "name" text NOT NULL,
+    "continent" text NOT NULL,
+    "region" text NOT NULL,
+    "surfacearea" float4 NOT NULL,
+    "indepyear" int2,
+    "population" int4 NOT NULL,
+    "lifeexpectancy" float4,
+    "gnp" numeric(10,2),
+    "gnpold" numeric(10,2),
+    "localname" text NOT NULL,
+    "governmentform" text NOT NULL,
+    "headofstate" text,
+    "capital" int4,
+    "code2" bpchar(2) NOT NULL,
+    PRIMARY KEY ("code")
+);
+
+-- Se inserta toda la info de una tabla en otra
+insert into country_bk (select * from country)
+
+-- Borramos la reestriccion para modifciar los datos en country
+alter table country  drop constraint country_continent_check;
