0e74727ebabf52bc401a42823bb8ce0476f77b7f
AdrianC47 <luiscabreraucme@hotmail.com>
2024-11-17 00:15:12 -0500

    Clausula - UNION

---
+++ Seccion_7_Joins-Uniones/Seccion_7_Joins_Uniones.sql
@@ -0,0 +1,12 @@
+-- USO DE UNION
+
+select code, name, '123' from continent where name like '%America%'
+union
+select 1,'otra cosa',name from continent where code in (3,5)
+order by  name asc;
+
+-- es importante que se tengan el mismo numero de columnas entre las partes a unirse
+-- se debe mantener el mismo tipo de dato pero dependiendo lo que se necesite
+-- puede variar el orden
+-- se puede usar el 1, 2 a manera de obtener los valores por orden , por ejemplo en el caso de
+-- arriba el 1 haría referencia a code
